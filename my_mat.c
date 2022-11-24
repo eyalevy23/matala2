565d35ce2b231f263d44634350c14e2f1e99f6d8
eyalevy23 <eyalevy23@gmail.com>
2022-11-24 18:07:32 +0200

    some changes

--- my_mat.c
+++ my_mat.c
@@ -1,9 +1,20 @@
 #include "my_mat.h"
 #include <stdio.h>
 
+int getMin(int a, int b){
+    if(a == 0){
+        return b;
+    }
+    if(b == 0){
+        return a;
+    }
+    if(a<b){
+        return a;
+    } return b;
+}
+
 void f1(int N, int (*mtx)[N][N]){
-    int a, b, sum;
-    for (int k = 0; k < N; k++) {
+    for (int k = 0; k <= N; k++) {
             // Pick all vertices as source one by one
             for (int i = 0; i < N; i++) {
                 // Pick all vertices as destination for the
@@ -12,12 +23,17 @@ void f1(int N, int (*mtx)[N][N]){
                     // If vertex k is on the shortest path from
                     // i to j, then update the value of
                     // dist[i][j]
-                    a = (*mtx)[i][k];
-                    b = (*mtx)[k][j];
-                    sum = a + b;
-                    if (a == 0 || b ==0) sum = 0;
-                    if (sum < (*mtx)[i][j])
-                        (*mtx)[i][j] = sum;
+                    // if ((*mtx)[i][k] + (*mtx)[k][j] < (*mtx)[i][j])
+                    //     (*mtx)[i][j] = (*mtx)[i][k] + (*mtx)[k][j];
+                    int inb1 = (*mtx)[i][k];
+                    int inb2 = (*mtx)[k][j];
+                    int sum = inb1 + inb2;
+                    if(inb1 == 0 || inb2 == 0) sum = 0;
+                    int min = getMin((*mtx)[i][j], sum);
+                    if(i == j){
+                        min = 0;
+                    }
+                    (*mtx)[i][j] = min;
                 }
             }
         }
